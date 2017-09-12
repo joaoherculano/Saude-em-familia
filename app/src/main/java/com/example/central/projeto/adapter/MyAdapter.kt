d71df0c37894b979910056a16971df0c8824a7ad
João Herculano <joaoherculanoo@gmail.com>
2017-09-12 00:22:22 -0300

    Adição de telas

---
+++ app/src/main/java/com/example/central/projeto/adapter/MyAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.central.projeto.adapter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.central.projeto.R
+
+class MyAdapter : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_adapter)
+    }
+}
---
+++ app/src/main/java/com/example/central/projeto/models/evento.kt
@@ -0,0 +1,14 @@
+package com.example.central.projeto.models
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.central.projeto.R
+
+class evento : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.itemevento)
+    }
+}
